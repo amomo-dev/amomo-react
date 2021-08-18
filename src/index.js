edc62ef0a0c4c5fd5ea12aa2b78fc4fe865bf858
chacha912 <dkwkdnwk912@gmail.com>
2021-08-19 03:23:15 +0900

    # ADD - 기본 스타일, sass utils 스타일 설정
    - noto-sans 폰트 추가
    - sass utils 스타일 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import 'styles/index.scss';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <StrictMode>
-    <div>amomo-react</div>
+    <App />
   </StrictMode>,
   document.getElementById('root')
 );
