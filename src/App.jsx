2a55cf496defce147483e27f93e0b10ed64a5b8e
chacha912 <dkwkdnwk912@gmail.com>
2021-08-20 03:57:29 +0900

    # ADD - SideNav 컴포넌트 마크업&스타일 적용 resolve #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { AlertBar } from 'components';
-import { Header, Main, Footer } from 'containers';
+import { Header, Main, Footer, SideNav } from 'containers';
 
 export default function App() {
   return (
@@ -7,6 +7,7 @@ export default function App() {
       <AlertBar noticeText="방문해 주셔서 감사합니다. 현재 개발중이기 때문에 기능에 제한이 있습니다." />
       <Header />
       <Main />
+      <SideNav />
       <Footer />
     </>
   );
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export * from './Header/Header';
 export * from './Main/Main';
+export * from './SideNav/SideNav';
 export * from './Footer/Footer';
