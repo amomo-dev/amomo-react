5bc269ab7faa9d3958b7928e29bbe904bde0f5cc
chacha912 <dkwkdnwk912@gmail.com>
2021-08-21 18:07:47 +0900

    # EDIT - 팀등록 페이지에 Input, Select 컴포넌트 적용

--- src/pages/MakeTeam/MakeTeam.js
+++ src/pages/MakeTeam/MakeTeam.js
@@ -1,24 +1,9 @@
 import styles from './MakeTeam.module.scss';
 
 import { useState } from 'react';
-import { A11yHidden, Button } from 'components';
+import { A11yHidden, Button, Input, Select } from 'components';
 
-const games = [
-  { id: 0, name: '오버워치' },
-  { id: 1, name: 'LOL' },
-  { id: 2, name: '동물의 숲' },
-  { id: 3, name: '마인크래프트' },
-  { id: 4, name: '기타' },
-];
-
-const ranks = [
-  { id: 0, name: '랭크 없음' },
-  { id: 1, name: '브론즈' },
-  { id: 2, name: '실버' },
-  { id: 3, name: '골드' },
-  { id: 4, name: '플래티넘' },
-  { id: 5, name: '다이아(이상)' },
-];
+import { games, ranks, memCount, publicSetting } from './select_option';
 
 export function MakeTeam() {
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(false);
@@ -29,82 +14,61 @@ export function MakeTeam() {
       <form className={styles.team_regist_form}>
         <fieldset>
           <A11yHidden as="legend">팀 등록 폼</A11yHidden>
-          <div className={`${styles.input_wrap} ${styles.full}`}>
-            <label htmlFor="teamName" className={styles.input_label}>
-              팀명
-            </label>
-            <input
-              type="text"
-              name="teamName"
-              id="teamName"
-              className={styles.input_text}
-              placeholder="4 ~ 10 자로 입력해주세요"
-              minLength="4"
-              maxLength="10"
-              required
-              autoFocus={true}
-            />
-          </div>
-          <div className={`${styles.input_wrap} ${styles.full}`}>
-            <label htmlFor="teamTitle" className={styles.input_label}>
-              제목
-            </label>
-            <input
-              type="text"
-              name="teamTitle"
-              id="teamTitle"
-              className={styles.input_text}
-              placeholder="4 ~ 30 자로 입력해주세요"
-              minLength="4"
-              maxLength="30"
-              required
-            />
-          </div>
-          <div className={styles.input_wrap}>
-            <label htmlFor="game" className={styles.input_label}>
-              게임
-            </label>
-            <select id="game" className={styles.select_box}>
-              {games.map(({ id, name }) => (
-                <option value={id} key={id}>
-                  {name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.input_wrap}>
-            <label htmlFor="teamMemCnt" className={styles.input_label}>
-              팀 인원 수
-            </label>
-            <select id="teamMemCnt" className={styles.select_box}>
-              {Array.from({ length: 8 }, (_, i) => i + 2).map((count) => (
-                <option value={count} key={count}>
-                  {count}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.input_wrap}>
-            <label htmlFor="rank" className={styles.input_label}>
-              랭크
-            </label>
-            <select id="rank" className={styles.select_box}>
-              {ranks.map(({ id, name }) => (
-                <option value={id} key={id}>
-                  {name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.input_wrap}>
-            <label htmlFor="public" className={styles.input_label}>
-              공개
-            </label>
-            <select id="public" className={styles.select_box}>
-              <option value={true}>공개</option>
-              <option value={false}>비공개</option>
-            </select>
-          </div>
+          <Input
+            id="teamName"
+            label="팀명"
+            labelClassName={styles.input_label}
+            placeholder="4 ~ 10 자로 입력해주세요"
+            className={`${styles.input_wrap} ${styles.full}`}
+            inputClassName={styles.input_text}
+            minLength="4"
+            maxLength="10"
+            required
+            autoFocus={true}
+          />
+          <Input
+            id="teamTitle"
+            label="팀명"
+            labelClassName={styles.input_label}
+            placeholder="4 ~ 30 자로 입력해주세요"
+            className={`${styles.input_wrap} ${styles.full}`}
+            inputClassName={styles.input_text}
+            minLength="4"
+            maxLength="30"
+            required
+          />
+          <Select
+            id="game"
+            label="게임"
+            optionData={games}
+            className={styles.input_wrap}
+            labelClassName={styles.input_label}
+            selectClassName={styles.select_box}
+          />
+          <Select
+            id="teamMemCnt"
+            label="팀 인원 수"
+            optionData={memCount}
+            className={styles.input_wrap}
+            labelClassName={styles.input_label}
+            selectClassName={styles.select_box}
+          />
+          <Select
+            id="rank"
+            label="랭크"
+            optionData={ranks}
+            className={styles.input_wrap}
+            labelClassName={styles.input_label}
+            selectClassName={styles.select_box}
+          />
+          <Select
+            id="public"
+            label="공개"
+            optionData={publicSetting}
+            className={styles.input_wrap}
+            labelClassName={styles.input_label}
+            selectClassName={styles.select_box}
+          />
           <Button
             type="submit"
             className={styles.submit_btn}
---
+++ src/pages/MakeTeam/select_option.js
@@ -0,0 +1,32 @@
+export const games = [
+  { id: 0, name: '오버워치' },
+  { id: 1, name: 'LOL' },
+  { id: 2, name: '동물의 숲' },
+  { id: 3, name: '마인크래프트' },
+  { id: 4, name: '기타' },
+];
+
+export const ranks = [
+  { id: 0, name: '랭크 없음' },
+  { id: 1, name: '브론즈' },
+  { id: 2, name: '실버' },
+  { id: 3, name: '골드' },
+  { id: 4, name: '플래티넘' },
+  { id: 5, name: '다이아(이상)' },
+];
+
+export const memCount = [
+  { id: 2, name: 2 },
+  { id: 3, name: 3 },
+  { id: 4, name: 4 },
+  { id: 5, name: 5 },
+  { id: 6, name: 6 },
+  { id: 7, name: 7 },
+  { id: 8, name: 8 },
+  { id: 9, name: 9 },
+];
+
+export const publicSetting = [
+  { id: 0, name: '공개' },
+  { id: 1, name: '비공개' },
+];
