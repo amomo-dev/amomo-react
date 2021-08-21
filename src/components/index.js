3ba819475d22b8f8d6c50fd953d878294a2e0213
chacha912 <dkwkdnwk912@gmail.com>
2021-08-21 18:06:18 +0900

    # ADD - Input component 생성

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from './A11yHidden/A11yHidden';
 export * from './Button/Button';
+export * from './Input/Input';
 export * from './AlertBar/AlertBar';
 export * from './TeamCard/TeamCard';
